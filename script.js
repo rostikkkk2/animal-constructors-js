ee40881615d704984e8b8eeaa379cdbaa517e98f
Rostik Yuriev <rost4402255@gmail.com>
2017-11-25 20:58:31 +0200

    ready animal-constr

---
+++ script.js
@@ -0,0 +1,15 @@
+function animal(sound_animal, number_of_paws, is_wild) {
+  this.sound_animal = sound_animal,
+  this.number_of_paws = number_of_paws,
+  this.is_wild = is_wild,
+
+  this.saySound = function() {
+    for (var i = 0; i < 3; i++) {
+      alert(this.sound_animal);
+    }
+  }
+}
+
+var animal_sound = new animal("gav", 4, true);
+
+animal_sound.saySound();
